e582fddfa386b763c00a70235c871f50b5a0b517
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-02-15 15:33:55 +0900

    Add empty proc_macro

--- direct_c_lang/src/lib.rs
+++ direct_c_lang/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn c_lang(tokens: TokenStream) -> TokenStream {
+    c_lang_impl(tokens)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn c_lang_impl(tokens: TokenStream) -> TokenStream {
+    todo!()
 }
