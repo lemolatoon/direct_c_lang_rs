a70b9ea5bf453f7db169da2e87d4efb7d7180d71
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-02-15 23:38:08 +0900

    [wip] impl c_lang macro

--- direct_c_lang/src/lib.rs
+++ direct_c_lang/src/lib.rs
@@ -1,10 +1,30 @@
+use ironcc::preprocess_and_compile;
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro]
 pub fn c_lang(tokens: TokenStream) -> TokenStream {
     c_lang_impl(tokens)
 }
 
+#[cfg(target_arch = "x86_64")]
 fn c_lang_impl(tokens: TokenStream) -> TokenStream {
-    todo!()
+    let src = tokens.to_string();
+    let start = "r#\"".to_string();
+    let end = "\"#".to_string();
+    let asm = preprocess_and_compile(src).unwrap_or_else(|err| panic!("{:?}", err));
+    quote!(
+        global_asm!(
+            r#"syscall"#
+        )
+    ).into()
+    // quote!(
+    //     global_asm!(
+    //         #start #asm #end
+    //     )
+    // ).into()
+}
+#[cfg(not(target_arch = "x86_64"))]
+fn c_lang_impl(tokens: TokenStream) -> TokenStream {
+    panic!("only support x86_64 arch");
 }
--- direct_c_lang_sample/src/main.rs
+++ direct_c_lang_sample/src/main.rs
@@ -1,3 +1,23 @@
+use direct_c_lang::c_lang;
+use std::{arch::global_asm, ffi::c_int};
+
+extern "C" {
+    fn fib(n: c_int) -> c_int;
+}
+
 fn main() {
     println!("Hello, world!");
 }
+
+c_lang!{
+    int fib(int n) {
+        if (n < -1) {
+            return -1;
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+};
